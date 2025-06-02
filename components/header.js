aeb16d6aee021b6cc795fc8d33081f078bd3ac89
M arif Irfan <marifirfannnn@gmail.com>
2025-06-02 13:17:19 +0700

    delete node

--- components/header.js
+++ components/header.js
@@ -20,15 +20,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
             <nav id="main-nav" class="hidden md:block mt-4">
                 <ul class="flex flex-col md:flex-row md:flex-wrap justify-start md:justify-center gap-4 lg:gap-8 xl:gap-20 text-left text-sm md:text-base">
-              <li><a href="/" class="...">Beranda</a></li>
-              <li><a href="/pages/profile.html" class="...">Profil</a></li>
-              <li><a href="/pages/perpustakaan.html" class="...">Perpustakaan</a></li>
-              <li><a href="/pages/kearsipan.html" class="...">Kearsipan</a></li>
-              <li><a href="/pages/informasi-publik.html" class="...">Informasi Publik</a></li>
-              <li><a href="/pages/berita.html" class="...">Berita</a></li>
-              <li><a href="/pages/baca-buku.html" class="...">Baca Buku</a></li>
-              <li><a href="https://e-rema.biz.id" target="_blank" class="...">Reservasi</a></li>
-
+                    <li><a href="/" class="text-forest-dark hover:text-forest-dark hover:font-semibold transition">Beranda</a></li>
+                    <li><a href="/pages/profile.html" class="text-forest-dark hover:text-forest-dark hover:font-semibold transition">Profil</a></li>
+                    <li><a href="/pages/perpustakaan.html" class="text-forest-dark hover:text-forest-dark hover:font-semibold transition">Perpustakaan</a></li>
+                    <li><a href="/pages/kearsipan.html" class="text-forest-dark hover:text-forest-dark hover:font-semibold transition">Kearsipan</a></li>
+                    <li><a href="/pages/informasi-publik.html" class="text-forest-dark hover:text-forest-dark hover:font-semibold transition">Informasi Publik</a></li>
+                    <li><a href="/pages/berita.html" class="text-forest-dark hover:text-forest-dark hover:font-semibold transition">Berita</a></li>
+                    <li><a href="/pages/baca-buku.html" class="text-forest-dark hover:text-forest-dark hover:font-semibold transition">Baca Buku</a></li>
+                    <li><a href="https://e-rema.biz.id" target="_blank" class="text-forest-dark hover:text-forest-dark hover:font-semibold transition">Reservasi</a></li>
                 </ul>
             </nav>
         </div>
