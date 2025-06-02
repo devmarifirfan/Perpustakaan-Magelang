a19186a046b8a19e608aaf0a774a4ab21590e2b5
M arif Irfan <marifirfannnn@gmail.com>
2025-06-02 12:57:05 +0700

    change route

--- public/components/header.js
+++ public/components/header.js
@@ -20,14 +20,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
             <nav id="main-nav" class="hidden md:block mt-4">
                 <ul class="flex flex-col md:flex-row md:flex-wrap justify-start md:justify-center gap-4 lg:gap-8 xl:gap-20 text-left text-sm md:text-base">
-                    <li><a href="/public/index.html" class="text-forest-dark hover:text-forest-dark hover:font-semibold transition">Beranda</a></li>
-                    <li><a href="/pages/profile.html" class="text-forest-dark hover:text-forest-dark hover:font-semibold transition">Profil</a></li>
-                    <li><a href="/pages/perpustakaan.html" class="text-forest-dark hover:text-forest-dark hover:font-semibold transition">Perpustakaan</a></li>
-                    <li><a href="/pages/kearsipan.html" class="text-forest-dark hover:text-forest-dark hover:font-semibold transition">Kearsipan</a></li>
-                    <li><a href="/pages/informasi-publik.html" class="text-forest-dark hover:text-forest-dark hover:font-semibold transition">Informasi Publik</a></li>
-                    <li><a href="/pages/berita.html" class="text-forest-dark hover:text-forest-dark hover:font-semibold transition">Berita</a></li>
-                    <li><a href="/pages/baca-buku.html" class="text-forest-dark hover:text-forest-dark hover:font-semibold transition">Baca Buku</a></li>
-                    <li><a href="https://e-rema.biz.id" target="_blank" class="text-forest-dark hover:text-forest-dark hover:font-semibold transition">Reservasi</a></li>
+              <li><a href="/public/" class="...">Beranda</a></li>
+              <li><a href="/public/profile.html" class="...">Profil</a></li>
+              <li><a href="/public/perpustakaan.html" class="...">Perpustakaan</a></li>
+              <li><a href="/public/kearsipan.html" class="...">Kearsipan</a></li>
+              <li><a href="/public/informasi-publik.html" class="...">Informasi Publik</a></li>
+              <li><a href="/public/berita.html" class="...">Berita</a></li>
+              <li><a href="/public/baca-buku.html" class="...">Baca Buku</a></li>
+              <li><a href="https://e-rema.biz.id" target="_blank" class="...">Reservasi</a></li>
+
                 </ul>
             </nav>
         </div>
